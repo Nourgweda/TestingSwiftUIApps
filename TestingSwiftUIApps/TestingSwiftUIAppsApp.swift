0314a8e87f8b1f12902d1935466db633f78ba663
Nourgweda <37792672+Nourgweda@users.noreply.github.com>
2023-06-30 16:31:38 +0300

    Initial Commit

---
+++ TestingSwiftUIApps/TestingSwiftUIAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingSwiftUIAppsApp.swift
+//  TestingSwiftUIApps
+//
+//  Created by Nour Gweda on 30/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestingSwiftUIAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
